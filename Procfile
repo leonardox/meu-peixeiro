da5b14c802266a1d7a3e4b296997589cb6b96d5c
Leonardo <lcxcordeiro@gmail.com>
2015-09-16 00:55:56 -0300

    last chance

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn meupeixeiro.wsgi:app --log-file -
+web: gunicorn meupeixeiro.wsgi
--- meupeixeiro/meupeixeiro/wsgi.py
+++ meupeixeiro/meupeixeiro/wsgi.py
@@ -1,15 +1,8 @@
-"""
-WSGI config for meupeixeiro project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meupeixeiro.settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
