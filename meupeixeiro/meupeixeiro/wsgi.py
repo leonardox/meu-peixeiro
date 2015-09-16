d031a4fe5b968aa54db946048383297f2f58d674
Leonardo <lcxcordeiro@gmail.com>
2015-09-16 01:05:23 -0300

    last chance

--- meupeixeiro/meupeixeiro/wsgi.py
+++ meupeixeiro/meupeixeiro/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meupeixeiro.meupeixeiro.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
