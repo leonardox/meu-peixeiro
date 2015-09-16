36f866ba90235c87bbec4ac60bef5a1890e7eb89
Leonardo <lcxcordeiro@gmail.com>
2015-09-16 00:26:32 -0300

    adding database

--- meupeixeiro/meupeixeiro/settings.py
+++ meupeixeiro/meupeixeiro/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default='postgres://rnyvpqrsmeulwz:IRyPL-Cj3-Si-tGqWyAQc2jydz@ec2-54-204-20-164.compute-1.amazonaws.com:5432/d1ude27bu5vagk')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
